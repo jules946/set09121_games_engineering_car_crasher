944b9bda175dec844b5e43a3c610a93075191cfa
Julius Manowski <114394404+jules946@users.noreply.github.com>
2024-12-04 16:47:57 +0000

    Adding ecm and maths libraries

---
+++ lib_ecm/ecm.cpp
@@ -0,0 +1,85 @@
+#include "ecm.h"
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <vector>
+
+using namespace sf;
+using namespace std;
+
+// Entity class implementation
+
+Entity::Entity()
+    : _position(Vector2f(0.0f, 0.0f)), _rotation(0.0f), _alive(true), _visible(true), _fordeletion(false) {}
+
+Entity::~Entity() = default;
+
+void Entity::update(double dt) {
+    for (auto &comp : _components) {
+        if (!comp->is_fordeletion()) {
+            comp->update(dt);
+        }
+    }
+    _components.erase(remove_if(_components.begin(), _components.end(),
+                                [](const shared_ptr<Component> &comp) {
+                                    return comp->is_fordeletion();
+                                }),
+                      _components.end());
+}
+
+void Entity::render() {
+    if (!_visible)
+        return;
+    for (const auto &comp : _components) {
+        comp->render();
+    }
+}
+
+const Vector2f &Entity::getPosition() const {
+    return _position;
+}
+
+void Entity::setPosition(const Vector2f &position) {
+    _position = position;
+}
+
+bool Entity::is_fordeletion() const {
+    return _fordeletion;
+}
+
+float Entity::getRotation() const {
+    return _rotation;
+}
+
+void Entity::setRotation(float rotation) {
+    _rotation = rotation;
+}
+
+bool Entity::isAlive() const {
+    return _alive;
+}
+
+void Entity::setAlive(bool alive) {
+    _alive = alive;
+}
+
+void Entity::setForDelete() {
+    _fordeletion = true;
+}
+
+bool Entity::isVisible() const {
+    return _visible;
+}
+
+void Entity::setVisible(bool visible) {
+    _visible = visible;
+}
+
+// Component class implementation
+
+Component::Component(Entity *const p) : _parent(p), _fordeletion(false) {}
+
+Component::~Component() = default;
+
+bool Component::is_fordeletion() const {
+    return _fordeletion;
+}
---
+++ lib_ecm/ecm.h
@@ -0,0 +1,80 @@
+//"ecm.h"
+#pragma once
+#include "maths.h"
+#include <memory>
+#include <vector>
+
+class Component; //forward declare
+
+class Entity {
+
+protected:
+    std::vector<std::shared_ptr<Component>> _components;
+    sf::Vector2f _position;
+    float _rotation;
+    bool _alive;       // should be updated
+    bool _visible;     // should be rendered
+    bool _fordeletion; // should be deleted
+public:
+    Entity();
+    virtual ~Entity();
+    virtual void update(double dt);
+    virtual void render();
+
+    const sf::Vector2f &getPosition() const;
+    void setPosition(const sf::Vector2f &_position);
+    bool is_fordeletion() const;
+    float getRotation() const;
+    void setRotation(float _rotation);
+    bool isAlive() const;
+    void setAlive(bool _alive);
+    void setForDelete();
+    bool isVisible() const;
+    void setVisible(bool _visible);
+
+    template <typename T, typename... Targs>
+    std::shared_ptr<T> addComponent(Targs... params) {
+        static_assert(std::is_base_of<Component, T>::value, "T != component");
+        std::shared_ptr<T> sp(std::make_shared<T>(this, params...));
+        _components.push_back(sp);
+        return sp;
+    }
+    template <typename T>
+    const std::vector<std::shared_ptr<T>> get_components() const {
+        static_assert(std::is_base_of<Component, T>::value, "T != component");
+        std::vector<std::shared_ptr<T>> ret;
+        for (const auto c : _components) {
+            if (typeid(*c) == typeid(T)) {
+                ret.push_back(std::dynamic_pointer_cast<T>(c));
+            }
+        }
+        return std::move(ret);
+    }
+    template <typename T>
+    const std::vector<std::shared_ptr<T>> GetCompatibleComponent() {
+        static_assert(std::is_base_of<Component, T>::value, "T != component");
+        std::vector<std::shared_ptr<T>> ret;
+        for (auto c : _components) {
+            auto dd = dynamic_cast<T*>(&(*c));
+            if (dd) {
+                ret.push_back(std::dynamic_pointer_cast<T>(c));
+            }
+        }
+        return ret;
+    }
+};
+
+class Component {
+protected:
+    Entity *const _parent;
+    bool _fordeletion; // should be removed
+    explicit Component(Entity *p);
+
+public:
+    Component() = delete;
+    bool is_fordeletion() const;
+    virtual void update(double dt) = 0;
+    virtual void render() = 0;
+    virtual ~Component();
+};
+
